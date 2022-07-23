85c00bf89c86fe0c37622c9cf3f8b8d8754eef52
v-aguiar <v.aguiar.dev@gmail.com>
2022-07-23 13:51:38 -0300

    feat: add seeding from prisma to populate db with initial values

---
+++ prisma/seed.ts
@@ -0,0 +1,172 @@
+﻿import { prisma } from "../src/config/db.js";
+
+async function _termUpsert() {
+  await prisma.term.upsert({
+    where: { number: 1 },
+    update: {},
+    create: { number: 1 },
+  });
+  await prisma.term.upsert({
+    where: { number: 2 },
+    update: {},
+    create: { number: 2 },
+  });
+  await prisma.term.upsert({
+    where: { number: 3 },
+    update: {},
+    create: { number: 3 },
+  });
+  await prisma.term.upsert({
+    where: { number: 4 },
+    update: {},
+    create: { number: 4 },
+  });
+  await prisma.term.upsert({
+    where: { number: 5 },
+    update: {},
+    create: { number: 5 },
+  });
+  await prisma.term.upsert({
+    where: { number: 6 },
+    update: {},
+    create: { number: 6 },
+  });
+}
+
+async function _categoryUpsert() {
+  await prisma.category.upsert({
+    where: { name: "Projeto" },
+    update: {},
+    create: { name: "Projeto" },
+  });
+  await prisma.category.upsert({
+    where: { name: "Prática" },
+    update: {},
+    create: { name: "Prática" },
+  });
+  await prisma.category.upsert({
+    where: { name: "Recuperação" },
+    update: {},
+    create: { name: "Recuperação" },
+  });
+}
+
+async function _teacherUpsert() {
+  await prisma.teacher.upsert({
+    where: { name: "Diego Pinho" },
+    update: {},
+    create: { name: "Diego Pinho" },
+  });
+  await prisma.teacher.upsert({
+    where: { name: "Bruna Hamori" },
+    update: {},
+    create: { name: "Bruna Hamori" },
+  });
+}
+
+async function _disciplineUpsert() {
+  await prisma.discipline.upsert({
+    where: { name: "HTML e CSS" },
+    update: {},
+    create: {
+      name: "HTML e CSS",
+      termId: 1,
+    },
+  });
+  await prisma.discipline.upsert({
+    where: { name: "Javascript" },
+    update: {},
+    create: {
+      name: "Javascript",
+      termId: 2,
+    },
+  });
+  await prisma.discipline.upsert({
+    where: { name: "React" },
+    update: {},
+    create: {
+      name: "React",
+      termId: 3,
+    },
+  });
+  await prisma.discipline.upsert({
+    where: { name: "Humildade" },
+    update: {},
+    create: {
+      name: "Humildade",
+      termId: 1,
+    },
+  });
+  await prisma.discipline.upsert({
+    where: { name: "Planejamento" },
+    update: {},
+    create: {
+      name: "Planejamento",
+      termId: 2,
+    },
+  });
+  await prisma.discipline.upsert({
+    where: { name: "Autoconfiaça" },
+    update: {},
+    create: {
+      name: "Autoconfiaça",
+      termId: 3,
+    },
+  });
+}
+
+async function _teacherDisciplineDeleteAndInsert() {
+  await prisma.$executeRaw`DELETE FROM "teacherDisciplines"`;
+
+  await prisma.teacherDiscipline.create({
+    data: {
+      teacherId: 1,
+      disciplineId: 1,
+    },
+  });
+  await prisma.teacherDiscipline.create({
+    data: {
+      teacherId: 1,
+      disciplineId: 3,
+    },
+  });
+  await prisma.teacherDiscipline.create({
+    data: {
+      teacherId: 1,
+      disciplineId: 4,
+    },
+  });
+  await prisma.teacherDiscipline.create({
+    data: {
+      teacherId: 2,
+      disciplineId: 5,
+    },
+  });
+  await prisma.teacherDiscipline.create({
+    data: {
+      teacherId: 2,
+      disciplineId: 6,
+    },
+  });
+  await prisma.teacherDiscipline.create({
+    data: {
+      teacherId: 2,
+      disciplineId: 7,
+    },
+  });
+}
+
+(async () => {
+  await _termUpsert();
+  await _categoryUpsert();
+  await _teacherUpsert();
+  await _disciplineUpsert();
+  await _teacherDisciplineDeleteAndInsert();
+})()
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
---
+++ src/routes/testRouter.ts
@@ -0,0 +1,5 @@
+﻿import { Router } from "express";
+
+const testRouter = Router();
+
+export default testRouter;
